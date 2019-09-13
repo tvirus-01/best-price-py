67d580c2c14ca90c7aa5701ce6796812f251772a
tvirus-01 <shakisa54@gmail.com>
2019-09-13 16:49:03 +0600

    django ready

---
+++ pages/choices.py
@@ -0,0 +1,19 @@
+site_choices = {
+    'amazon': 'Amazon',
+    'ebay': 'Ebay',
+    'newegg': 'Newegg',
+    'walmart': 'Walmart',
+    'bestbuy': 'BestBuy',
+    'target': 'Target',
+    'jet': 'Jet'
+}
+
+site_logo_choices = [
+    'img/logo/amazon.png',
+    'img/logo/ebay.png',
+    'img/logo/walmart.png',
+    'img/logo/newegg.png',
+    'img/logo/bestbuy.png',
+    'img/logo/target.png',
+    'img/logo/jet.png'
+]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse as hre
+from django.template import Context, Template 
+from .choices import site_choices, site_logo_choices
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'site_choices': site_choices,
+        'site_logo_choices' : site_logo_choices
+    }
+    return render(request, 'pages/index.html', context)
+
+def search(request):
+    return render(request, 'pages/search.html')
